e883550528fc42866ea54e661404d508ca9e1ebb
FuYaoDe <dan82625@gmail.com>
2017-01-08 22:57:06 +0800

    add app intro

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import { connect } from 'react-redux';
+import Intro from './Intro';
 import Login from './Login';
 import Home from './Home';
 import Detail from './Detail';
@@ -12,9 +13,10 @@ export default class AppRoute extends React.Component {
     return (
       <RouterWithRedux>
         <Scene key="root">
+          <Scene key="intro" hideNavBar component={Intro} initial/>
           <Scene key="login" hideNavBar component={Login} title="登入" />
-          <Scene key="home" component={Home} title="Home" initial/>
-          <Scene key="cardDetail" component={Detail} />
+          <Scene key="home" hideNavBar={false} hideBackImage={true} onBack={() => {}} component={Home} title="Home" />
+          <Scene key="cardDetail" hideNavBar={false} component={Detail} />
         </Scene>
       </RouterWithRedux>
     );
